5d18013e6f332bb30dd04c9d83f6a4ff3d970a92
velfibra <devlopment@clickspeed.net.br>
2025-04-29 10:56:50 -0300

    style: refact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,15 @@ export default function Home() {
   const posts = getAllPosts();
 
   return (
-    <div className="bg-gradient-to-bl from-[#1A0530] via-[#48088b] to-[#1A0530] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-gradient-to-bl h-full from-[#1A0530] via-[#48088b] to-[#1A0530] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div />
       <section className="w-full max-w-5xl">
         <h2 className="text-2xl font-bold mb-4 text-center text-white">
           Últimos Posts
         </h2>
-        <PostSwiper posts={posts} />
+        <div>
+          <PostSwiper posts={posts} />
+        </div>
       </section>
       <div />
     </div>
